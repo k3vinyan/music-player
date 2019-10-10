6a1a0195400ca0b4b556d01ddc886d240183e8c4
Kevin Yan <k3vinyan@gmail.com>
2019-10-10 02:12:36 -0700

    player is able to play audio

--- src/components/Musicplayer.vue
+++ src/components/Musicplayer.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="player-container">
-        <div v-for="song in player.songs" :key="song.id" class="card" :style="{backgroundImage: 'url(' + song.artist_image_medium +')'}">
+        <div v-for="song in player.songs" :key="song.id" class="card" :class="{ hide: showCard(song.id) }" :style="{backgroundImage: 'url(' + song.artist_image_medium +')'}">
             <img class="artist" :src="song.artist_image_medium" />
         </div>
         
-        <img class="svg-icon" :src="musicIcon.prev" />
+        <img class="svg-icon" @click="decreaseIndex" :src="musicIcon.prev" />
         <img class="svg-icon" @click="play" :src="musicIcon.play" />
-        <img class="svg-icon" :src="musicIcon.next" />
+        <img class="svg-icon" @click="increaseIndex" :src="musicIcon.next" />
     </div>
 
 </template>
@@ -22,6 +22,8 @@ import prevSVG  from '../../public/images/previous.svg';
 const player = new Player();
 player.fetchSongs();
 
+
+
 export default {
     name: 'musicplayer',
     data: function() {
@@ -32,20 +34,38 @@ export default {
                 pause: pauseSVG,
                 prev: prevSVG,
                 next: nextSVG
-            }
+            },
+            currentActiveIndex: 1
         }
     },
     methods: {
-        play: function(audio) {          
-            this.player.play(audio)
+        play: function() {
+            const src = this.player.songs[this.currentActiveIndex - 1].track1_preview;
+            const audio = new Audio(src);
+            audio.play();
         },
         pause: function(audio) {
         this.player.pause(audio)
+        },
+        showCard: function(id) {
+            if(id !== this.currentActiveIndex) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        decreaseIndex: function() {
+            if(this.currentActiveIndex === 1) {
+                return;
+            }
+            this.currentActiveIndex--;
+        },
+        increaseIndex: function() {
+            this.currentActiveIndex++;
+
         }
+
        
-    },
-    mounted: function() {
-        
     }
 }
 </script>
@@ -57,6 +77,15 @@ export default {
         width:  500px;
         height: 300px;
     }
+    .player-container {
+        display: flex;
+    }
+
+
+    .hide {
+        display: none;
+    }
+
     .artist {
         width: 200px;
         height: 200px;
